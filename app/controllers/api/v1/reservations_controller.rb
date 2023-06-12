d3d1af99b8c7a07789e404e9d9800bb0bdd0a5b8
Hassaanjbaig-code <80938128+Hassaanjbaig-code@users.noreply.github.com>
2023-06-12 21:29:02 +0500

    Add show and create

--- app/controllers/api/v1/reservations_controller.rb
+++ app/controllers/api/v1/reservations_controller.rb
@@ -3,4 +3,22 @@ class ReservationsController < ApplicationController
     user = User.find_by(id: session[:user_id])
     @reservations = user.reservations
   end
+  def show
+    @reservation = Reservation.find_by(id: params[:id])
+  end
+
+  def create
+    @reservation = Reservation.new(reservation_params)
+    if @reservation.save
+      redirect_to reservations_path
+    else
+      render :new
+    end
+  end
+
+  private 
+
+  def reservation_params
+    params.require(:reservation).permit(:start_date, :end_date)
+  end
 end
