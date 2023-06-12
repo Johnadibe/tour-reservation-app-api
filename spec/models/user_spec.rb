334f442353690d23c070f81b204e3c57f25755af
Donmark2k <chukwuemekavp@gmail.com>
2023-06-12 21:04:23 +0100

    Ensure that tour_spec test is passing

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 RSpec.describe User, type: :model do
   before(:each) do
     @user = User.create(name: 'Test user', email: 'rails@gmail.com',
-      password_digest: '123456')
+                        password_digest: '123456')
   end
   it 'is valid with valid attributes' do
     expect(@user).to be_valid
@@ -16,9 +16,9 @@ RSpec.describe User, type: :model do
     @user.email = nil
     expect(@user).to_not be_valid
   end
- 
+
   it 'is not valid with a name shorter than 3 characters' do
-    @user.name = 01
+    @user.name = 0o1
     expect(@user).to_not be_valid
   end
 end
