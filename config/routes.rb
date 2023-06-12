c87bf8da250822545cb2b87fc31a44fac57f9674
Hassaanjbaig-code <80938128+Hassaanjbaig-code@users.noreply.github.com>
2023-06-12 21:27:39 +0500

    Add controller reservation

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  get 'reservations/index'
+  get 'reservations/show'
+  get 'reservations/create'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
