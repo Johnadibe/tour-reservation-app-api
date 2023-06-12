9d02c35d465b8e0e14d740958e8682d06864944e
mrprotocoll <lekanvgbg@gmail.com>
2023-06-12 19:42:36 +0100

    Add action to generate token in application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,5 @@
 class ApplicationController < ActionController::API
+    def generate_token(user)
+        JWT.encode({ user_id: user.id }, Rails.application.credentials.jwt_key, 'HS256')
+    end
 end
