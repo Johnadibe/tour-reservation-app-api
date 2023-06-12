c282d737fa7d9fad9862e26e374bd9894b4973b7
mrprotocoll <lekanvgbg@gmail.com>
2023-06-12 19:46:57 +0100

    Add action to decode token

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,19 @@
 class ApplicationController < ActionController::API
-    def generate_token(user)
-        JWT.encode({ user_id: user.id }, Rails.application.credentials.jwt_key, 'HS256')
+    def jwt_key
+        Rails.application.credentials.jwt_key
     end
+
+    def generate_token(user)
+        JWT.encode({ user_id: user.id }, jwt_key, 'HS256')
+    end
+
+    def decoded_token
+        begin
+            token = request.headers['Authorization']&.split(' ')&.last
+            JWT.decode(token, jwt_key, true, { :algorithm => 'HS256' })
+        rescue JWT::DecodeError
+            [{error: "Invalid Token"}]
+        end    
+    end
+
 end
