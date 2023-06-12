ae2a53836172bcf11b3c2edac21de20f463aaa9d
John Adibe <johnadibe123@gmail.com>
2023-06-12 14:15:37 -0800

    Add associations and validations to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,8 @@
 class User < ApplicationRecord
+    has_many :reservations, dependent: :destroy
+    has_many :tours, through: :reservations
+
+    validates :name, presence: true
+    validates :email, presence: true, uniqueness: true
+    validates :password, presence: true, length: { minimum: 6 }
 end
