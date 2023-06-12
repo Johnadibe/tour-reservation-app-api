31fd879ca7406674d11f7ab533ff9ae6a620bcd5
John Adibe <johnadibe123@gmail.com>
2023-06-12 14:27:37 -0800

    Fix rubocop errors

--- app/models/tour.rb
+++ app/models/tour.rb
@@ -1,6 +1,6 @@
 class Tour < ApplicationRecord
   has_one_attached :image
-  
+
   belongs_to :user
 
   has_many :reservations, dependent: :destroy
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,8 @@
 class User < ApplicationRecord
-    has_many :reservations, dependent: :destroy
-    has_many :tours, through: :reservations
+  has_many :reservations, dependent: :destroy
+  has_many :tours, through: :reservations
 
-    validates :name, presence: true
-    validates :email, presence: true, uniqueness: true
-    validates :password, presence: true, length: { minimum: 6 }
+  validates :name, presence: true
+  validates :email, presence: true, uniqueness: true
+  validates :password, presence: true, length: { minimum: 6 }
 end
