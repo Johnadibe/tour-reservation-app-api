bae4daf6669c00bec0f937a728491d093f7b75e7
mrprotocoll <lekanvgbg@gmail.com>
2023-06-13 11:25:28 +0100

    Add action to authenticate user during login

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -8,8 +8,7 @@ class Api::V1::UsersController < ApplicationController
   def create
     user = User.new(user_params)
     if user.save
-        token = issue_token(user)
-        render json: {user: UserSerializer.new(user), token: token}
+        render json: {data: UserSerializer.new(user), message: "Registration successful" token: generate_token(user)}
     else
         if user.errors.messages
             render json: {error: user.errors.messages}
@@ -19,10 +18,24 @@ class Api::V1::UsersController < ApplicationController
     end
   end
 
+  def authenticate
+    user = User.find_by_email(session_params[:email])
+
+    if user.present? && user.authenticate(session_params[:password])
+        render json: {user: UserSerializer.new(user), message: "Logged in successfully" token: generate_token(user)}
+    else
+        render json: {error: "Incorrect username or password."}
+    end
+  end
+
   private
 
   def user_params
       params.permit(:name, :email, :password)
+  end
+
+  def session_params
+    params.permit(:email, :password)
   end
 
 end
