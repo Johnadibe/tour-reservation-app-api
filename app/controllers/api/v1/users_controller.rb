f17a7e5f471d0aea5d20a89fbfeec30a15443762
mrprotocoll <lekanvgbg@gmail.com>
2023-06-13 11:13:35 +0100

    Add create action to register new user

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -5,4 +5,24 @@ class Api::V1::UsersController < ApplicationController
     render json: User.all
   end
 
+  def create
+    user = User.new(user_params)
+    if user.save
+        token = issue_token(user)
+        render json: {user: UserSerializer.new(user), token: token}
+    else
+        if user.errors.messages
+            render json: {error: user.errors.messages}
+        else
+            render json:  {error: "User could not be created. Try again"}
+        end
+    end
+  end
+
+  private
+
+  def user_params
+      params.permit(:name, :email, :password)
+  end
+
 end
