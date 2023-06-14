e07399a8588d7e72d7253215f1ea3e224094b1d7
Hassaanjbaig-code <80938128+Hassaanjbaig-code@users.noreply.github.com>
2023-06-14 22:14:48 +0500

    Add more test for the users

--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -20,6 +20,16 @@ RSpec.describe Tour, type: :request do
         expect(json_data['data']['email']).to eq('rails1@yopmail.com')
         expect(json_data['token']).not_to be_nil
     end
-
+    scenario '/api/v1/users/:id' do
+        get "/api/v1/users/#{json_data['data']['id']}", headers: { 'Authorization' => "Bearer #{json_data['token']}" }
+        expect(response.status).to eq(200)
+        expect(response).to have_http_status(:ok)
+    end
+    scenario '/api/v1/users/' do
+        get "/api/v1/users", headers: { 'Authorization' => "Bearer #{json_data['token']}" }
+        expect(response.status).to eq(200)
+        expect(response).to have_http_status(:ok)
+        expect(response.body.length).not_to be_nil
+    end
   end
 end
