a3fbb5d487a04ea15f6bc12538e726bfde1c479c
John Adibe <johnadibe123@gmail.com>
2023-06-14 12:54:49 -0800

    Add index method in tours_controller

--- app/controllers/api/v1/tours_controller.rb
+++ app/controllers/api/v1/tours_controller.rb
@@ -1,2 +1,9 @@
 class Api::V1::ToursController < ApplicationController
+    skip_before_action :authenticate_user!, only: [:index]
+
+    def index 
+        @tours = Tour.all
+        render json: @tours
+    end
+
 end
