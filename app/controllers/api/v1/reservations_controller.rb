7a4570c8e1dfa67f8b493ce054affd019f816cf8
Hassaanjbaig-code <80938128+Hassaanjbaig-code@users.noreply.github.com>
2023-06-14 17:49:35 +0500

    Fix some more issue relative to the reservation

--- app/controllers/api/v1/reservations_controller.rb
+++ app/controllers/api/v1/reservations_controller.rb
@@ -2,16 +2,15 @@ class Api::V1::ReservationsController < ApplicationController
   def index
     user = current_user
     @reservations = Reservation.includes(:tour).where(user_id: user.id)
-    render json: @reservations
+    render json: @reservations.to_json(include: :tour)
   end
 
   def show
     @reservation = Reservation.find_by(id: params[:id])
-    render json: @reservation
+    render json: @reservation.to_json(include: :tour)
   end
 
   def create
-    # @user = current_user
     @reservation = current_user.reservations.new(reservation_params)
     if @reservation.save
       render json: @reservation, status: :created, notice: 'Reservation created successfully'
