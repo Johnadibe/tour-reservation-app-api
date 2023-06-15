19e77c19faffa60f5477b7676dbae9e11e92cf8f
Hassaanjbaig-code <80938128+Hassaanjbaig-code@users.noreply.github.com>
2023-06-15 17:42:32 +0500

    Made the tour test

--- spec/requests/tours_spec.rb
+++ spec/requests/tours_spec.rb
@@ -1,34 +1,56 @@
 require 'rails_helper'
 
 RSpec.describe 'Tours', type: :request do
+  before(:each) do
+    post '/api/v1/users', params: {
+      name: 'test3',
+      email: 'rails3@yopmail.com',
+      password: 'backend12'
+    }
+  end
+
+  let(:json_data) { JSON.parse(response.body) }
   
   describe 'POST /api/v1/tours' do
-    before(:each) do
-      post '/api/v1/users', params: {
-        name: 'test3',
-        email: 'rails3@yopmail.com',
-        password: 'backend12'
-      }
-    end
-  
-    let(:json_data) { JSON.parse(response.body) }
-    it 'create a tour' do
+    scenario 'create a tour' do
       token = json_data['token']
-      puts "Token: #{token}" # Add this line for debugging
-
-      post '/api/v1/tours', params: {
+      
+      post "/api/v1/tours?token=#{token}", params: {
         tour: {
-          name: 'Joyland',
-          city: 'Lahore',
+          name: "Joyland",
+          city: "Lahore",
           price: 30,
-          video: 'This is the video',
-          image: 'This is the image'
+          video: "This is the video",
+          image: "This is the image"
         }
-      }, headers:  { 'Authorization' => "Bearer #{json_data['token']}" }
-
-      puts "Response: #{response.body}" # Add this line for debugging
-
-      expect(response.body).to not_be_nil
+      }
+      
+      
+      expect(response.body).not_to be_nil
     end
+    scenario 'checks the request when we are not passing the token' do
+      post "/api/v1/tours", params: {
+        tour: {
+          name: "Joyland",
+          city: "Lahore",
+          price: 30,
+          video: "This is the video",
+          image: "This is the image"
+        }
+      }
+      expect(response.status).to eq(401)
+    end    
+    scenario 'checks the request ' do
+      post "/api/v1/tours", params: {
+        tour: {
+          name: "Joyland",
+          city: "Lahore",
+          price: 30,
+          video: "This is the video",
+          image: "This is the image"
+        }
+      }
+      expect(response).not_to have_http_status(:ok)
+    end    
   end
 end
