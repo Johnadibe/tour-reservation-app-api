e9f38aefa1a2d6ec8aded3f2c54e9050b3bb3328
John Adibe <johnadibe123@gmail.com>
2023-06-16 14:44:17 -0800

    Update routes.rb file

--- config/routes.rb
+++ config/routes.rb
@@ -11,7 +11,7 @@ Rails.application.routes.draw do
       resources :users, only: [:create, :show, :index]
       post "/login", to: "users#login"
       get "/me", to: "users#me"
-      resources :tours, except: [:new, :edit]
+      resources :tours, except: [:new, :edit, :destroy]
       resources :reservations, only: [:create, :show, :index]
     end
   end
