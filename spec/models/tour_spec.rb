6957e1a62f62401a09c56e2a928f96abec33865e
mrprotocoll <lekanvgbg@gmail.com>
2023-06-20 22:23:27 +0100

    Update reservation and tour model test

--- spec/models/tour_spec.rb
+++ spec/models/tour_spec.rb
@@ -7,8 +7,9 @@ RSpec.describe Tour, type: :model do
     @tour = Tour.create(name: 'Obudu tour', city: 'Ibom', price: 245.00, video: 'Do it now or die regretting',
                         des: 'This is the small desription of the Obudu tour that we have plan', user_id: @user.id)
   end
+  let!(:tour) { create :tour }
   it 'is valid with valid attributes' do
-    expect(@tour).to be_valid
+    expect(tour).to be_valid
   end
   it 'is not valid without a name' do
     @tour.name = nil
